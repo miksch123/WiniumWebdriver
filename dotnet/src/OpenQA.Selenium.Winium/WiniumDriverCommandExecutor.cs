c2fdd3609504fe43915b225f5f8d550ddcf64318
miksch123 <44267886+miksch123@users.noreply.github.com>
2019-10-14 09:31:59 +0200

    initial commit

---
+++ dotnet/src/OpenQA.Selenium.Winium/WiniumDriverCommandExecutor.cs
@@ -0,0 +1,73 @@
+﻿namespace OpenQA.Selenium.Winium
+{
+    #region using
+
+    using System;
+    using System.Reflection;
+
+    using OpenQA.Selenium.Remote;
+
+    #endregion
+
+    public class WiniumDriverCommandExecutor : ICommandExecutor
+    {
+        #region Fields
+
+        private ICommandExecutor internalExecutor;
+
+        private WiniumDriverService service;
+
+        #endregion
+
+        #region Constructors and Destructors
+
+        public WiniumDriverCommandExecutor(WiniumDriverService driverService, TimeSpan commandTimeout)
+        {
+            this.service = driverService;
+            this.internalExecutor = CommandExecutorFactory.GetHttpCommandExecutor(driverService.ServiceUrl, commandTimeout);
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        public CommandInfoRepository CommandInfoRepository
+        {
+            get
+            {
+                return this.internalExecutor.CommandInfoRepository;
+            }
+        }
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        public Response Execute(Command commandToExecute)
+        {
+            if (commandToExecute == null)
+            {
+                throw new ArgumentNullException("commandToExecute", "Command may not be null");
+            }
+
+            if (commandToExecute.Name == DriverCommand.NewSession)
+            {
+                this.service.Start();
+            }
+
+            try
+            {
+                return this.internalExecutor.Execute(commandToExecute);
+            }
+            finally
+            {
+                if (commandToExecute.Name == DriverCommand.Quit)
+                {
+                    this.service.Dispose();
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ scripts/ci.ps1
@@ -0,0 +1,23 @@
+﻿Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+#------------------------------
+
+$root = $PWD
+
+# .NET
+cd ..\dotnet\scripts
+& .\ci.ps1
+if ($LASTEXITCODE -ne 0)
+{
+    Write-Output ".NET build failed. See console output"
+    Exit $LASTEXITCODE
+}
+
+# Java
+cd $root\..\java\scripts
+& .\ci.ps1
+if ($LASTEXITCODE -ne 0)
+{
+    Write-Output "Java build failed. See console output"
+    Exit $LASTEXITCODE
+}
